4bc152b3f7d3490a68be7c8bb1b291ad9bfa1b75
Marsu5 <marcell.schuh@gmail.com>
2016-10-07 15:58:04 +0200

    MyButton Start

---
+++ core/src/hu/tokingame/MyButton.java
@@ -0,0 +1,14 @@
+package hu.tokingame;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+
+/**
+ * Created by M on 10/7/2016.
+ */
+
+public class MyButton extends TextButton {
+    public MyButton(String text, Skin skin) {
+        super(text, skin);
+    }
+}
