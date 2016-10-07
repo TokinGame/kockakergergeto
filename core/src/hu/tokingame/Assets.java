a7d41e9ffe79ffc414dff3fec26395b98006c4b0
Marsu5 <marcell.schuh@gmail.com>
2016-10-07 13:35:54 +0200

    Assetes

--- core/src/hu/tokingame/Assets.java
+++ core/src/hu/tokingame/Assets.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 
 /**
  * Created by M on 10/7/2016.
@@ -15,6 +16,8 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 public class Assets {
 
     public static AssetManager mgr;
+
+
 
     public static void prepare() {
         mgr = new AssetManager();
